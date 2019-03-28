1a86c46cc5076d31cbc8336b91eb7685e4f0d960
Wang,Mike M <mike.wang@student.fontys.nl>
2019-03-28 15:17:52 +0100

    - added unit tests project - added collection, game, user classes

---
+++ Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class User
+    {
+        public string Name { get; private set; }
+        public Date DatePublished { get; private set; }
+        public string Description { get; private set; }
+        public string DownloadLink { get; private set; }
+        public List<string> Tags { get; private set; }
+
+        public User(string name, Date datePublished, string description, string downloadLink, List<string> tags)
+        {
+            Name = name;
+            DatePublished = datePublished;
+            Description = description;
+            DownloadLink = downloadLink;
+            Tags = tags;
+        }
+    }
+}
