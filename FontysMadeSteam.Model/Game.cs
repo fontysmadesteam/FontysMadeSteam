5dc2b7cf02859bf7f9ba63339b26f89e13f7fdfd
Wang,Mike M <mike.wang@student.fontys.nl>
2019-04-04 11:04:53 +0200

    Rechanged to .NET Core libraries(my fault)

--- FontysMadeSteam.Model/Game.cs
+++ FontysMadeSteam.Model/Game.cs
@@ -7,12 +7,12 @@ namespace Models
     public class Game
     {
         public string Name { get; set; }
-        public Date DatePublished { get; set; }
+        public DateTime DatePublished { get; set; }
         public string Description { get; set; }
         public string DownloadLink { get; set; }
         public List<string> Tags { get; set; }
 
-        public Game(string name, Date datePublished, string description, string downloadLink, List<string> tags)
+        public Game(string name, DateTime datePublished, string description, string downloadLink, List<string> tags)
         {
             Name = name;
             DatePublished = datePublished;
