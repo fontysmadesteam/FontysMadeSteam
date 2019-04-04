00cdad4f5a2a3cb966ff9cb23a3e6af2d4b08cde
MartinRuis <ruismartin98@gmail.com>
2019-04-04 09:31:27 +0200

    Started gamelogic class

---
+++ FontysMadeSteam.Logic/GameLogic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Models;
+
+namespace FontysMadeSteam.Logic
+{
+    public class GameLogic
+    {
+        public Game GetGame(int game_id)
+        {
+            throw new NotImplementedException();
+        } 
+    }
+}
