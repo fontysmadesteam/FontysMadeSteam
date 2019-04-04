f6066bad549f6cbff0cc88183f9e3d48e6eac80a
Wang,Mike M <mike.wang@student.fontys.nl>
2019-04-04 11:48:07 +0200

    added game repository class

---
+++ FontysMadeSteam.DAL/Repository/GameRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FontysMadeSteam.DAL.Repository
+{
+    public class GameRepository
+    {
+        public 
+    }
+}
