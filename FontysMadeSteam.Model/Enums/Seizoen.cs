3a6a5c41061ff0ac08eaeecec22727774841cb26
Wang,Mike M <mike.wang@student.fontys.nl>
2019-05-23 10:42:12 +0200

    uitgave reform

---
+++ FontysMadeSteam.Model/Enums/Seizoen.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FontysMadeSteam.Model.Enums
+{
+    public enum Seizoen
+    {
+        Najaar,
+        Voorjaar
+    }
+}
